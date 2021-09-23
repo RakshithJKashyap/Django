67e546ac6c4a41b2d6604ce32521040dfe4453cf
Rakshith J <rakshithj07@gmail.com>
2021-09-23 19:45:48 +0530

    database connected

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projectsApp.apps.ProjectsappConfig'
+    'projectsApp.apps.ProjectsappConfig',
 ]
 
 MIDDLEWARE = [
